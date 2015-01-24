f1cb5ba520a82a6afcbbaea63282ab3e216ba46a
Freddy Ramirez <2santosxd@gmail.com>
2015-01-23 22:09:49 -0530

    problema directorios

--- ProyectoBd/src/com/Proyecto/interfaz/VentanaCaja.java
+++ ProyectoBd/src/com/Proyecto/interfaz/VentanaCaja.java
@@ -14,289 +14,275 @@ import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.table.DefaultTableModel;
 
-
-
-public class VentanaCaja  extends JFrame implements ActionListener {
+public class VentanaCaja extends JFrame implements ActionListener {
 
 	public VentanaCaja() {
 
-	initGUI();
-	
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setTitle("Gestor de licoreria");
+		initGUI();
 
-	setSize(800,600);
-	setVisible(true);
-	setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Gestor de licoreria");
+
+		setSize(800, 600);
+		setVisible(true);
+		setResizable(false);
 
 	}
-	
+
 	private void initGUI() {
 
 		getContentPane().setLayout(new GridBagLayout());
 		getContentPane().setBackground(Color.WHITE);
 		GridBagConstraints config = new GridBagConstraints();
-		
-		JLabel etiqueta1 = new JLabel(" Caja Registradora");
-		etiqueta1.setFont(new java.awt.Font("Arial",Font.BOLD, 34));
-		config.gridx=1;
-		config.gridy=1;
-		config.gridheight=3;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.weightx=0;
-		config.fill=GridBagConstraints.BOTH;
-		config.anchor=GridBagConstraints.NORTHWEST;
-		getContentPane().add(etiqueta1,config);
-	
-		/********************** Creando La tabla**************************************/
-		JTable tabla=new JTable();
-		   String[] columnas = {"Nombre", "Cantidad","Precio"};
-		      
-		   DefaultTableModel modelo = new DefaultTableModel();
-		   JScrollPane desplazamiento = new JScrollPane(tabla);
-		   
-	        //int id, consola, obtenido,cantidad,catidadMin;
-	        //float precio,presentacion;
-	        //String nombre;
-	        
-	        // Modelo de la tabla
-	        modelo.setColumnIdentifiers(columnas);
-	        
-	        // Barras de desplazamiento
-	        desplazamiento.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-	        desplazamiento.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-	        
-	        // Propiedades de la tabla
-	        tabla.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-	        tabla.setFillsViewportHeight(true);        
-	        tabla.setModel(modelo);
-	        
-	     // Agregando elementos a la ventana
-	        
-	    config.gridx=1;
-		config.gridy=8;
-		config.gridheight=3;
-		config.gridwidth=4;
-		config.weighty=1;
-		config.weightx=1;
-		config.fill=GridBagConstraints.BOTH;
-		config.anchor=GridBagConstraints.NORTHWEST;
-		getContentPane().add(desplazamiento,config);
-		
-		
-		/*********************** Creando El Formulario*******************/
-		
-		JLabel etiqueta2 = new JLabel("Cedula Cliente");
-		config.gridx=7;
-		config.gridy=4;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(etiqueta2,config);
-		
-		JTextField txtCedula = new JTextField("");
-		config.gridx=7;
-		config.gridy=5;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(txtCedula,config);
-		
-		
-		JLabel etiqueta3 = new JLabel("Codigo ");
-		config.gridx=6;
-		config.gridy=7;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weightx=0;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(etiqueta3,config);
-		
-		JTextField txtNombre = new JTextField("");
-		config.gridx=6;
-		config.gridy=8;
-		config.gridheight=1;
-		config.gridwidth=1;
-		//config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(txtNombre,config);
-		
-		JLabel etiqueta4 = new JLabel("Nombre Producto");
-		config.gridx=7;
-		config.gridy=7;
-		config.gridheight=1;
-		config.gridwidth=1;
-		//config.weightx=0;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(etiqueta4,config);
-		
-		JTextField txtNombreProd = new JTextField("");
-		config.gridx=7;
-		config.gridy=8;
-		config.gridheight=1;
-		config.gridwidth=1;
-		//config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(txtNombreProd,config);
-		
-		JLabel etiqueta5 = new JLabel("Cantidad");
-		config.gridx=7;
-		config.gridy=9;
-		config.gridheight=1;
-		config.gridwidth=1;
-		//config.weightx=0;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(etiqueta5,config);
-		
-		JTextField txtCantidad = new JTextField("");
-		config.gridx=7;
-		config.gridy=10;
-		config.gridheight=1;
-		config.gridwidth=1;
-		//config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(txtCantidad,config);
-		
-		
-		/*********************** Creando los botones*******************/
-		
-		//boton para Buscar Cliente
-		JButton botonBuscarCliente = new JButton("Buscar Cliente");
-		config.gridx=8;
-		config.gridy=5;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(botonBuscarCliente,config);
-		
-		JButton botonBuscarProd = new JButton("Buscar Producto");
-		config.gridx=8;
-		config.gridy=9;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(botonBuscarProd,config);
-		
-		JButton botonAgregarProd = new JButton("Agregar Producto");
-		config.gridx=8;
-		config.gridy=8;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(botonAgregarProd,config);
-		
-		//boton para eliminar un producto
-		JButton botonEliminarProd = new JButton("Eliminar Producto");
-		config.gridx=8;
-		config.gridy=10;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(botonEliminarProd,config);
-		
-		
-		JButton botonImprimir = new JButton("Imprimir Factura");
-		config.gridx=4;
-		config.gridy=14;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(botonImprimir,config);
-		
-		// Boton para volver
-		JButton botonVolver = new JButton("Volver");
-		config.gridx=5;
-		config.gridy=14;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=0;
-		config.fill=GridBagConstraints.HORIZONTAL;
-		getContentPane().add(botonVolver,config);
-		
-		
-		/*********************** logo de la aplicacion*******************/
 
-		ImageIcon imagen = new ImageIcon("Assets/logoPeq.png"); 
+		JLabel etiqueta1 = new JLabel(" Caja Registradora");
+		etiqueta1.setFont(new java.awt.Font("Arial", Font.BOLD, 34));
+		config.gridx = 1;
+		config.gridy = 1;
+		config.gridheight = 3;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.weightx = 0;
+		config.fill = GridBagConstraints.BOTH;
+		config.anchor = GridBagConstraints.NORTHWEST;
+		getContentPane().add(etiqueta1, config);
+
+		/********************** Creando La tabla **************************************/
+		JTable tabla = new JTable();
+		String[] columnas = { "Nombre", "Cantidad", "Precio" };
+
+		DefaultTableModel modelo = new DefaultTableModel();
+		JScrollPane desplazamiento = new JScrollPane(tabla);
+
+		// int id, consola, obtenido,cantidad,catidadMin;
+		// float precio,presentacion;
+		// String nombre;
+
+		// Modelo de la tabla
+		modelo.setColumnIdentifiers(columnas);
+
+		// Barras de desplazamiento
+		desplazamiento
+				.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		desplazamiento
+				.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+		// Propiedades de la tabla
+		tabla.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		tabla.setFillsViewportHeight(true);
+		tabla.setModel(modelo);
+
+		// Agregando elementos a la ventana
+
+		config.gridx = 1;
+		config.gridy = 8;
+		config.gridheight = 3;
+		config.gridwidth = 4;
+		config.weighty = 1;
+		config.weightx = 1;
+		config.fill = GridBagConstraints.BOTH;
+		config.anchor = GridBagConstraints.NORTHWEST;
+		getContentPane().add(desplazamiento, config);
+
+		/*********************** Creando El Formulario *******************/
+
+		JLabel etiqueta2 = new JLabel("Cedula Cliente");
+		config.gridx = 7;
+		config.gridy = 4;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(etiqueta2, config);
+
+		JTextField txtCedula = new JTextField("");
+		config.gridx = 7;
+		config.gridy = 5;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(txtCedula, config);
+
+		JLabel etiqueta3 = new JLabel("Codigo ");
+		config.gridx = 6;
+		config.gridy = 7;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weightx = 0;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(etiqueta3, config);
+
+		JTextField txtNombre = new JTextField("");
+		config.gridx = 6;
+		config.gridy = 8;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		// config.weighty=0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(txtNombre, config);
+
+		JLabel etiqueta4 = new JLabel("Nombre Producto");
+		config.gridx = 7;
+		config.gridy = 7;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		// config.weightx=0;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(etiqueta4, config);
+
+		JTextField txtNombreProd = new JTextField("");
+		config.gridx = 7;
+		config.gridy = 8;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		// config.weighty=0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(txtNombreProd, config);
+
+		JLabel etiqueta5 = new JLabel("Cantidad");
+		config.gridx = 7;
+		config.gridy = 9;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		// config.weightx=0;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(etiqueta5, config);
+
+		JTextField txtCantidad = new JTextField("");
+		config.gridx = 7;
+		config.gridy = 10;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		// config.weighty=0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(txtCantidad, config);
+
+		/*********************** Creando los botones *******************/
+
+		// boton para Buscar Cliente
+		final JButton botonBuscarCliente = new JButton("Buscar Cliente");
+		config.gridx = 8;
+		config.gridy = 5;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(botonBuscarCliente, config);
+
+		final JButton botonBuscarProd = new JButton("Buscar Producto");
+		config.gridx = 8;
+		config.gridy = 9;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(botonBuscarProd, config);
+
+		final JButton botonAgregarProd = new JButton("Agregar Producto");
+		config.gridx = 8;
+		config.gridy = 8;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(botonAgregarProd, config);
+
+		// boton para eliminar un producto
+		final JButton botonEliminarProd = new JButton("Eliminar Producto");
+		config.gridx = 8;
+		config.gridy = 10;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(botonEliminarProd, config);
+
+		final JButton botonImprimir = new JButton("Imprimir Factura");
+		config.gridx = 4;
+		config.gridy = 14;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(botonImprimir, config);
+
+		// Boton para volver
+		final JButton botonVolver = new JButton("Volver");
+		config.gridx = 5;
+		config.gridy = 14;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 0;
+		config.fill = GridBagConstraints.HORIZONTAL;
+		getContentPane().add(botonVolver, config);
+
+		/*********************** logo de la aplicacion *******************/
+
+		ImageIcon imagen = new ImageIcon("Assets/logoPeq.png");
 		JLabel imagenLabel = new JLabel();
 		imagenLabel.setIcon(imagen);
-		config.gridx=8;
-		config.gridy=14;
-		config.gridheight=4;
-		config.gridwidth=4;
-		config.weightx=0;
-		config.weighty=0;
-		config.anchor=GridBagConstraints.WEST;
-		getContentPane().add(imagenLabel,config);
-		
+		config.gridx = 8;
+		config.gridy = 14;
+		config.gridheight = 4;
+		config.gridwidth = 4;
+		config.weightx = 0;
+		config.weighty = 0;
+		config.anchor = GridBagConstraints.WEST;
+		getContentPane().add(imagenLabel, config);
+
 		/*********************** Manejando eventos *******************/
 
-	      ActionListener al = new ActionListener() 
-	      { 
-	         public void actionPerformed(ActionEvent evt) 
-	         { 
-	            Object obj = evt.getSource(); 
-	            if (obj == botonBuscarCliente) 
-	            	botonBuscarClienteActionPerformed(evt); 
-	            else if (obj == botonEliminarProd) 
-	            	botonEliminarProdActionPerformed(evt); 
-	            else if (obj == botonVolver) 
-	            	botonVolverActionPerformed(evt); 
-	         } 
-	      }; 
-	      botonBuscarCliente.addActionListener(al); 
-	      botonEliminarProd.addActionListener(al); 
-	      botonVolver.addActionListener(al); 
+		ActionListener al = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent evt) {
+				Object obj = evt.getSource();
+				if (obj == botonBuscarCliente)
+					botonBuscarClienteActionPerformed(evt);
+				else if (obj == botonEliminarProd)
+					botonEliminarProdActionPerformed(evt);
+				else if (obj == botonVolver)
+					botonVolverActionPerformed(evt);
+			}
+		};
+		botonBuscarCliente.addActionListener(al);
+		botonEliminarProd.addActionListener(al);
+		botonVolver.addActionListener(al);
 
-        
-}
-	
-	
-	//acciones al precionar los botones
-	   private void botonBuscarClienteActionPerformed(ActionEvent evt) 
-	   { 
+	}
 
-	   } 
+	// acciones al precionar los botones
+	private void botonBuscarClienteActionPerformed(ActionEvent evt) {
 
-	   private void botonEliminarProdActionPerformed(ActionEvent evt) 
-	   { 
+	}
 
-	   } 
+	private void botonEliminarProdActionPerformed(ActionEvent evt) {
 
-	   private void botonVolverActionPerformed(ActionEvent evt) 
-	   { 
-			// TODO Auto-generated method stub
-			VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
-			ventanaPrincipal.setVisible(true);
-			this.dispose();
+	}
 
-	   } 
-		
-		
-	
+	private void botonVolverActionPerformed(ActionEvent evt) {
+		// TODO Auto-generated method stub
+		VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+		ventanaPrincipal.setVisible(true);
+		this.dispose();
+
+	}
 
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-			
-			VentanaCaja ventana;
-			ventana = new VentanaCaja();
-		
-			
-		}
+
+		VentanaCaja ventana;
+		ventana = new VentanaCaja();
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
-	}
+
+}
--- ProyectoBd/src/com/Proyecto/interfaz/VentanaPrincipal.java
+++ ProyectoBd/src/com/Proyecto/interfaz/VentanaPrincipal.java
@@ -13,209 +13,211 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 @SuppressWarnings("serial")
-public class VentanaPrincipal extends JFrame implements ActionListener{
+public class VentanaPrincipal extends JFrame implements ActionListener {
 
 	public VentanaPrincipal() {
 
-	initGUI();
-	
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setTitle("Gestor de licoreria");
-	setSize(800,600);
-	setVisible(true);
-	setResizable(false);
-	repaint();
+		initGUI();
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Gestor de licoreria");
+		setSize(800, 600);
+		setVisible(true);
+		setResizable(false);
+		repaint();
 	}
-	
+
 	private void initGUI() {
 
 		getContentPane().setLayout(new GridBagLayout());
 		getContentPane().setBackground(Color.WHITE);
 		GridBagConstraints config = new GridBagConstraints();
-		
+
 		JLabel etiqueta1 = new JLabel("Gestor de licoreria");
-		etiqueta1.setFont(new java.awt.Font("Arial",Font.BOLD, 34));
-		config.gridx=0;
-		config.gridy=0;
-		config.gridheight=1;
-		config.gridwidth=2;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(etiqueta1,config);
-		
-		
-		JButton botonCaja= new JButton("Caja");
-		config.gridx=0;
-		config.gridy=1;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(botonCaja,config);
-		
-		JButton botonClientes = new JButton("Clientes");
-		config.gridx=0;
-		config.gridy=2;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(botonClientes,config);
-		
-		JButton botonVendedores = new JButton("Vendores");
-		config.gridx=0;
-		config.gridy=3;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		
-		getContentPane().add(botonVendedores,config);
-		
-		
-		JButton botonInventario = new JButton("Inventario");
-		config.gridx=0;
-		config.gridy=4;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		
-		getContentPane().add(botonInventario,config);
-		
-		JButton botonProeveedores = new JButton("Proveedores");
-		config.gridx=0;
-		config.gridy=5;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(botonProeveedores,config);
-		
-		JButton botonSalir = new JButton("Salir");
-		config.gridx=0;
-		config.gridy=6;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weighty=1;
-		config.fill=GridBagConstraints.BOTH;
-		getContentPane().add(botonSalir,config);
-		
-		
-		
+		etiqueta1.setFont(new java.awt.Font("Arial", Font.BOLD, 34));
+		config.gridx = 0;
+		config.gridy = 0;
+		config.gridheight = 1;
+		config.gridwidth = 2;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(etiqueta1, config);
+
+		final JButton botonCaja = new JButton("Caja");
+		config.gridx = 0;
+		config.gridy = 1;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(botonCaja, config);
+
+		final JButton botonClientes = new JButton("Clientes");
+		config.gridx = 0;
+		config.gridy = 2;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(botonClientes, config);
+
+		final JButton botonVendedores = new JButton("Vendores");
+		config.gridx = 0;
+		config.gridy = 3;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+
+		getContentPane().add(botonVendedores, config);
+
+		final JButton botonInventario = new JButton("Inventario");
+		config.gridx = 0;
+		config.gridy = 4;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+
+		getContentPane().add(botonInventario, config);
+
+		final JButton botonProeveedores = new JButton("Proveedores");
+		config.gridx = 0;
+		config.gridy = 5;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(botonProeveedores, config);
+
+		final JButton botonSalir = new JButton("Salir");
+		config.gridx = 0;
+		config.gridy = 6;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weighty = 1;
+		config.fill = GridBagConstraints.BOTH;
+		getContentPane().add(botonSalir, config);
+
 		JLabel labelEspaciador = new JLabel();
-		config.gridx=1;
-		config.gridy=1;
-		config.gridheight=1;
-		config.gridwidth=1;
-		config.weightx=1;
-		getContentPane().add(labelEspaciador,config);
-	
-		ImageIcon imagen = new ImageIcon("Assets/logo.png"); 
+		config.gridx = 1;
+		config.gridy = 1;
+		config.gridheight = 1;
+		config.gridwidth = 1;
+		config.weightx = 1;
+		getContentPane().add(labelEspaciador, config);
+
+		ImageIcon imagen = new ImageIcon("Assets/logo.png");
 		JLabel imagenLabel = new JLabel();
 		imagenLabel.setIcon(imagen);
-		config.gridx=2;
-		config.gridy=1;
-		config.gridheight=6;
-		config.gridwidth=3;
-		config.weightx=1;
-		config.anchor=GridBagConstraints.WEST;
-		getContentPane().add(imagenLabel,config);
-		
-		// Lisener de botones 
-		
+		config.gridx = 2;
+		config.gridy = 1;
+		config.gridheight = 6;
+		config.gridwidth = 3;
+		config.weightx = 1;
+		config.anchor = GridBagConstraints.WEST;
+		getContentPane().add(imagenLabel, config);
+
+		// Lisener de botones
+
 		class FormListener implements java.awt.event.ActionListener {
-			FormListener() {}
-				public void actionPerformed(java.awt.event.ActionEvent evt) {
-					if (evt.getSource() == botonClientes) {
-						VentanaPrincipal.this.botonClientesActionPerformed(evt);
-					}else{
-						if (evt.getSource() == botonVendedores) {
-							VentanaPrincipal.this.botonVendedoresActionPerformed(evt);
-						}else{
-							if (evt.getSource() == botonInventario) {
-								VentanaPrincipal.this.botonInventarioActionPerformed(evt);
-							}else{
-								if (evt.getSource() == botonProeveedores) {
-									VentanaPrincipal.this.botonProeveedoresActionPerformed(evt);
-								}else{
-									if (evt.getSource() == botonCaja) {
-										VentanaPrincipal.this.botonCajaActionPerformed(evt);
-									}else{
-										if (evt.getSource() == botonSalir) {
-											VentanaPrincipal.this.botonSalirActionPerformed(evt);
-										}
+			FormListener() {
+			}
+
+			@Override
+			public void actionPerformed(java.awt.event.ActionEvent evt) {
+				if (evt.getSource() == botonClientes) {
+					VentanaPrincipal.this.botonClientesActionPerformed(evt);
+				} else {
+					if (evt.getSource() == botonVendedores) {
+						VentanaPrincipal.this
+								.botonVendedoresActionPerformed(evt);
+					} else {
+						if (evt.getSource() == botonInventario) {
+							VentanaPrincipal.this
+									.botonInventarioActionPerformed(evt);
+						} else {
+							if (evt.getSource() == botonProeveedores) {
+								VentanaPrincipal.this
+										.botonProeveedoresActionPerformed(evt);
+							} else {
+								if (evt.getSource() == botonCaja) {
+									VentanaPrincipal.this
+											.botonCajaActionPerformed(evt);
+								} else {
+									if (evt.getSource() == botonSalir) {
+										VentanaPrincipal.this
+												.botonSalirActionPerformed(evt);
 									}
-							
 								}
+
+							}
 						}
 					}
 				}
 			}
 		}
-	FormListener formListener = new FormListener();	
-	
-	botonClientes.addActionListener(formListener);
-	botonVendedores.addActionListener(formListener);
-	botonInventario.addActionListener(formListener);
-	botonProeveedores.addActionListener(formListener);
-	botonCaja.addActionListener(formListener);
-	botonSalir.addActionListener(formListener);
+		FormListener formListener = new FormListener();
+
+		botonClientes.addActionListener(formListener);
+		botonVendedores.addActionListener(formListener);
+		botonInventario.addActionListener(formListener);
+		botonProeveedores.addActionListener(formListener);
+		botonCaja.addActionListener(formListener);
+		botonSalir.addActionListener(formListener);
 	}
-	
+
 	public void botonClientesActionPerformed(ActionEvent evt) {
 		// TODO Auto-generated method stub
-			VentanaClientes ventanaClientes = new VentanaClientes();
-			ventanaClientes.setVisible(true);
-			this.dispose();
+		VentanaClientes ventanaClientes = new VentanaClientes();
+		ventanaClientes.setVisible(true);
+		this.dispose();
 	}
-	
+
 	public void botonVendedoresActionPerformed(ActionEvent evt) {
 		// TODO Auto-generated method stub
-			VentanaVendedores ventanaVendedores= new VentanaVendedores();
-			ventanaVendedores.setVisible(true);
-			this.dispose();
+		VentanaVendedores ventanaVendedores = new VentanaVendedores();
+		ventanaVendedores.setVisible(true);
+		this.dispose();
 	}
-	
+
 	public void botonInventarioActionPerformed(ActionEvent evt) {
 		// TODO Auto-generated method stub
-			VentanaInventario ventanaInventario = new VentanaInventario();
-			ventanaInventario.setVisible(true);
-			this.dispose();
+		VentanaInventario ventanaInventario = new VentanaInventario();
+		ventanaInventario.setVisible(true);
+		this.dispose();
 	}
-	
+
 	public void botonProeveedoresActionPerformed(ActionEvent evt) {
 		// TODO Auto-generated method stub
-			VentanaProveedores ventanaProveedores = new VentanaProveedores();
-			//ventanaProveedores.setVisible(true);
-			this.dispose();
+		VentanaProveedores ventanaProveedores = new VentanaProveedores();
+		// ventanaProveedores.setVisible(true);
+		this.dispose();
 	}
-	
+
 	public void botonCajaActionPerformed(ActionEvent evt) {
 		// TODO Auto-generated method stub
-			VentanaCaja ventanaCaja = new VentanaCaja();
-			//ventanaCaja.setVisible(true);
-			this.dispose();
+		VentanaCaja ventanaCaja = new VentanaCaja();
+		// ventanaCaja.setVisible(true);
+		this.dispose();
 	}
-	
+
 	public void botonSalirActionPerformed(ActionEvent evt) {
-			this.dispose();
+		this.dispose();
 	}
-	
+
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-			
-			VentanaPrincipal ventana;
-			ventana = new VentanaPrincipal();
-		
-			
-		}
+
+		VentanaPrincipal ventana;
+		ventana = new VentanaPrincipal();
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
-	}
-	
+
 	}
 
+}
